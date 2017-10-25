48622f7fa39202adcc3a18e5e1275f491494c91f
Ígor Chagas <igorchagasm@hotmail.com>
2017-10-25 17:44:56 -0200

    Update web_server.lua
    fixes

--- servidor/web_server.lua
+++ servidor/web_server.lua
@@ -10,23 +10,27 @@ end
 --to receive from shellserver
 local receiver = assert(socket.bind("0.0.0.0", 2321))
 
---to send to shellclient
-local sender = assert(socket.tcp())
-sender:connect("0.0.0.0", 2322)
-
 
 while 1 do
   local requester = receiver:accept()
-  print("Connected: "..requester:getpeername())
   local line, err = requester:receive()
   requester:close()
 
   --check if line payload is a valid HTTP request
+  if line == "GET / HTTP/1.1" then
+    --build response
+    response = "HTTP/1.1 200 OK\n\n"..readAll("index.html")
+  else
+    response = "HTTP/1.1 400 Bad Request \n\nInvalid Request!"
+  end
 
-  --build response. Change later to a html file
-  local response = "HTTP/1.1 200 OK\n\n"..readAll("index.html")
-  print("linha: "..line)
+  print(line)
+  print("---------")
   print(response)
+  --to send to shellclient
+  local sender = assert(socket.tcp())
+  sender:connect("0.0.0.0", 2322)
   sender:send(response)
   sender:close()
+  print("---------")
 end
