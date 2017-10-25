4d74938ebce4e2c58be110406a36fc7c271bf3ff
Ígor Chagas <igorchagasm@hotmail.com>
2017-10-25 17:47:32 -0200

    Update web_client.lua

--- cliente/web_client.lua
+++ cliente/web_client.lua
@@ -3,40 +3,36 @@ local socket = require("socket")
 --to receive from shellserver
 local receiver = assert(socket.bind("0.0.0.0", 2325))
 
---to send to shellclient
-local sender = assert(socket.tcp())
-sender:connect("0.0.0.0", 2319)
-
+--to receive from chrome
 local web = assert(socket.bind("0.0.0.0", 2321))
 
 
 while 1 do
   --getting google chrome request
   local client = web:accept()
-  --print('connected: '..client:getpeername())
   local line, err = client:receive()
 
   if not err then
-    --send to shellclient
+    --to send to shellclient
+    local sender = assert(socket.tcp())
+    sender:connect("0.0.0.0", 2319)
     sender:send(line)
     sender:close()
 
     --get response from shellserver
     local requester = receiver:accept()
-    local line, err = requester:receive()
+
+    local line, err = requester:receive("*a")
+    requester:close()
     print(line)
-    local result = string.gsub(line, "200 OK", "200 OK\n\n", 1)
-    --print(result)
+    print("------------------------")
 
     --handle response
-    client:send(result)
-
-    client:close()
-    requester:close()
-
+    client:send(line)
   else
-    local response = "HTTP/1.1 200 OK\n\n DEU RUIM!"
+    local response = "HTTP/1.1 500 Internal Server Error"
     client:send(response)
-    client:close()
   end
+  client:close()
+
 end
